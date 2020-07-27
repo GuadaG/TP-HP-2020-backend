2e2c4806d1235dcdd2984156ba23649b7bfc364f
ivanteise <ivanteise@gmail.com>
2020-07-26 23:35:49 -0300

    agrego Service Instituciones

---
+++ Texere.Services/InstitucionesService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using Texere.DataAccess;
+using Texere.Model;
+using Texere.Service.Interfaces;
+
+namespace Texere.Service
+{
+    public class InstitucionesService : IInstitucionesService
+    {
+        private readonly TexereDbContext _texereDbContext;
+        public InstitucionesService(TexereDbContext texereDbContext)
+        {
+            _texereDbContext = texereDbContext;
+        }
+
+        public bool Add(Institucion model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Institucion model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Institucion Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Institucion> GetAll()
+        {
+            var result = new List<Institucion>();
+
+            try
+            {
+                result = _texereDbContext.Instituciones.ToList();
+            }
+            catch (System.Exception)
+            {
+
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Texere.Services/Interfaces/IInstitucionesService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Texere.Model;
+
+namespace Texere.Service.Interfaces
+{
+    public interface IInstitucionesService
+    {
+        bool Add(Institucion model);
+        bool Delete(int id);
+        bool Update(Institucion model);
+        Institucion Get(int id);
+        IEnumerable<Institucion> GetAll();
+    }
+}
