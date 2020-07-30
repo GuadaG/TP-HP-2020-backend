26d2953fdccb40b23ff381dbeea98186633ab2b5
Iván Teise <57991454+ivanteise@users.noreply.github.com>
2020-07-29 22:40:34 -0300

    Agregar color (#1)
    * agrego clase Colores a Model
    
    * agrego DbSet Colores a DbContext
    
    * agrego Service Colores
    
    * agrego Controller Colores
    
    * Se registró la dependencia en el Startup
    
    Co-authored-by: GuadaG <gueretag@gmail.com>

---
+++ Texere.Services/Interfaces/IColoresService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Texere.Model;
+
+namespace Texere.Service.Interfaces
+{
+    public interface IColoresService
+    {
+        bool Add(Colores model);
+        bool Delete(int id);
+        bool Update(Colores model);
+        Colores Get(int id);
+        IEnumerable<Colores> GetAll();
+    }
+}
---
+++ Texere.WebAPI/Controllers/ColoresController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Texere.Service.Interfaces;
+
+namespace Texere.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ColoresController : ControllerBase
+    {
+        private readonly IColoresService _coloresService;
+
+        public ColoresController(IColoresService coloresService)
+        {
+            _coloresService = coloresService;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(
+                _coloresService.GetAll()
+            );
+        }
+    }
+}
