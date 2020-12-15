e2e74945e664a6c026565791035d6cbc6150c118
Iván Teise <ivanteise@gmail.com>
2020-12-14 22:04:20 -0300

    DTOs modelos y accesorios

---
+++ Texere.WebAPI/DTOs/AccesoriosDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Texere.WebAPI.DTOs
+{
+    public class AccesoriosDTO
+    {
+        public int AccesorioId { get; set; }
+        public string DescAccesorio { get; set; }
+        public bool TieneTalle { get; set; }
+        public float Precio { get; set; }
+    }
+}
---
+++ Texere.WebAPI/DTOs/ModelosDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Texere.WebAPI.DTOs
+{
+    public class ModelosDTO
+    {
+        public int ModeloId { get; set; }
+        public string DescModelo { get; set; }
+        public string Imagen { get; set; }
+        public int ColorBaseId { get; set; }
+        public List<int> Instituciones { get; set; }
+        public List<int> Colores { get; set; }
+    }
+}
